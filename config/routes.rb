93776df3784592d289318e71b39e0dad8504b773
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2016-01-23 13:11:58 -0500

    What Am I Thinking?

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ root 'welcome#index'
 
 resources :reports, path: '/report'
 
+ get "about" => 'welcome#about'
+ get "contact" => 'welcome#contact'
+ get "plans" => 'welcome#plans'
+
 
 
 end
--- db/migrate/20160123160312_create_reports.rb
+++ db/migrate/20160123160312_create_reports.rb
@@ -3,6 +3,7 @@ class CreateReports < ActiveRecord::Migration
     create_table :reports do |t|
       t.string :title
       t.string :slug
+      
 
       t.timestamps null: false
     end
